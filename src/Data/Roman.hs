9bb5a06cc6c73510f6ca043b5361f169b19c70a6
Alexander Hakki <ahakki@ahakki.xyz>
2016-05-10 14:28:46 +0200

    add Data.Roman Module

---
+++ src/Data/Roman.hs
@@ -0,0 +1,16 @@
+-- Roman-hs
+
+{- |
+Module      :  $Header$
+Description :  Integral Datatype for Roman Numerals
+Copyright   :  (c) Alexander Hakki
+License     :  BSD3
+
+Maintainer  :  ahk@ahakki.xyz
+Stability   :  experimental
+Portability :  portable
+-}
+
+module Data.Roman 
+    (        
+    ) where
