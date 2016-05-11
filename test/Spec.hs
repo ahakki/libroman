e309c251e68bf01c945c23b1ed55ad415de747b7
Alexander Hakki <ahakki@ahakki.xyz>
2016-05-11 16:54:35 +0200

    better style

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,11 @@ import           Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
-        describe "Data.Roman" $
-            it "fromInteger to Roman and back gives same Result" $ property $
-                \x -> fromRoman (fromInteger x :: RomanList) == (x :: Integer) || (fromRoman ( fromInteger (negate x) :: RomanList ))  == (negate x :: Integer)
+   describe "Data.Roman" $
+    it "fromInteger to Roman and back gives same Result" $
+      property $
+        \ x ->
+          fromRoman (fromInteger x :: RomanList) == (x :: Integer) ||
+            fromRoman (fromInteger (negate x) :: RomanList) ==
+              (negate x :: Integer)
 
