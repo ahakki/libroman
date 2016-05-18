6d7922f8375c19facf114b7dea4f66fa73e3e833
Alexander Hakki <ahakki@ahakki.xyz>
2016-05-18 09:30:24 +0200

    add test for XIIX -> 18

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,10 +8,11 @@ import           Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
-   describe "Data.Roman" $
+   describe "Data.Roman" $ do
     it "fromInteger to Roman and back gives same Result" $
       property $
         \ x -> fromRoman (fromInteger x :: RomanList) == (x :: Integer)
             || fromRoman (fromInteger (negate x) :: RomanList)
             == (negate x :: Integer)
-
+    it "works with unconventional romans like XIIX -> 18" $ do
+      fromRoman ([X, I, I, X] :: RomanList) `shouldBe` 18
