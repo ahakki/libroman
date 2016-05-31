3bda1875d0c34bb2ea845e7d486f57de811ff028
Alexander Hakki <ahakki@ahakki.xyz>
2016-05-31 09:34:58 +0200

    add documentation

--- src/Data/Roman.hs
+++ src/Data/Roman.hs
@@ -14,5 +14,8 @@ module Data.Roman
     ( Roman (..)
     ) where
 
+{- |
+The Class Roman implements a single Method, fromRoman, to convert to an Integral Type"
+-}
 class Roman r where
     fromRoman :: Integral b =>  r -> b
