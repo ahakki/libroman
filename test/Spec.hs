892eb7d16e4adce40223a2ef50be31a46889f7e9
Alexander Hakki <ahakki@ahakki.xyz>
2016-06-14 14:09:33 +0200

    Squashed commit of the following:
    commit 056ee7458befc45378d01afff23958d737744dc4
    Author: Alexander Hakki <ahk@ahakki.xyz>
    Date:   Mon Jun 13 23:36:10 2016 +0200
    
        better tests
    
    commit 6f72309bb82c7a63427bf90b5ae1eee6501e1eae
    Author: Alexander Hakki <ahk@ahakki.xyz>
    Date:   Mon Jun 13 22:26:21 2016 +0200
    
        wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,37 @@
+--Spec.hs
+
 import           Data.Roman
 import           Test.Hspec
 import           Test.QuickCheck
 
-
+default (RomanNumeral)
 
 main :: IO ()
 main = hspec $ do
    describe "Data.Roman" $ do
-    it "fromInteger to Roman and back gives same Result" $
+    it "(fromRoman . fromInteger) = id" $ do
       property $
-        \ x -> fromRoman (fromInteger x :: RomanNumeral) == (x :: Integer)
-            || fromRoman (fromInteger (negate x) :: RomanNumeral)
-            == (negate x :: Integer)
-    it "works with unconventional romans like XIIX -> 18" $ do
-      fromRoman ([X, I, I, X] :: RomanNumeral) `shouldBe` 18
+        \ x ->
+             fromRoman (fromInteger x :: RomanNumeral) ==
+             (x :: Integer)
+          || fromRoman (fromInteger (negate x) :: RomanNumeral) ==
+             (negate x :: Integer)
+
+    it "(read . show) = id" $ do
+      property $
+        \ x ->
+             ((read . show) (fromInteger x :: RomanNumeral) :: RomanNumeral) ==
+             (fromInteger x :: RomanNumeral)
+
+    it "Unconventionally Written Romans have equal value" $ do
+             let unconvVal = fromRoman [X, I, I, X]    :: Integer
+             let convVal =   fromRoman [X, V, I, I, I] :: Integer
+             unconvVal `shouldBe` convVal
+
+    it "(Unconventional Roman == Conventional Roman) fails" $ do
+             let unconvVal = [X, I, I, X]
+             let convVal   = [X, V, I, I, I]
+             unconvVal == convVal `shouldBe` False
+
+    it "negative RomanNumerals are equal to their Positive equivalents" $ do
+             12 `shouldBe` (-12)
