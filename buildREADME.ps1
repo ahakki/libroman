b605cbb860051501cd7487f3b8db5738fef15888
Alexander Hakki <ahk@ahakki.xyz>
2016-06-16 22:41:34 +0200

    do one build always

--- buildREADME.ps1
+++ buildREADME.ps1
@@ -7,12 +7,15 @@ $watcher.IncludeSubdirectories = $false
 $watcher.EnableRaisingEvents = $false
 $watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
 
+write-host "initial build, now we watch";
+pandoc .\src\Data\Roman\Tutorial.lhs -f markdown+lhs -t markdown_github -o README.md;
+(Get-Content .\README.md).replace('sourceCode', 'haskell') | Set-Content .\README.md
+
 while($TRUE){
 	$result = $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed -bor [System.IO.WatcherChangeTypes]::Renamed -bOr [System.IO.WatcherChangeTypes]::Created, 1000);
 	if($result.TimedOut){
 		continue;
 	}
-    write-host "Change Detected, lets do dis";
     pandoc .\src\Data\Roman\Tutorial.lhs -f markdown+lhs -t markdown_github -o README.md;
     (Get-Content .\README.md).replace('sourceCode', 'haskell') | Set-Content .\README.md
 
