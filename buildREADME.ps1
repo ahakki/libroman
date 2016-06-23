e16a31b49762a609019b98c4eed4f589e6bf4985
Alexander Hakki <ahk@ahakki.xyz>
2016-06-23 22:25:55 +0200

    update

--- buildREADME.ps1
+++ buildREADME.ps1
@@ -7,9 +7,10 @@ $watcher.IncludeSubdirectories = $false
 $watcher.EnableRaisingEvents = $false
 $watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
 
-write-host "initial build, now we watch";
 pandoc .\src\Data\Roman\Tutorial.lhs -f markdown+lhs -t markdown_github -o README.md;
 (Get-Content .\README.md).replace('sourceCode', 'haskell') | Set-Content .\README.md
+write-host "initial build completed.";
+write-host "watching";
 
 while($TRUE){
 	$result = $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed -bor [System.IO.WatcherChangeTypes]::Renamed -bOr [System.IO.WatcherChangeTypes]::Created, 1000);
