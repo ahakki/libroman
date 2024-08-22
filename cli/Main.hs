ee10c839cceaca25029a6671b9c3a71b4fb31ed9
A <ahk@ahakki.xyz>
2024-08-22 03:37:10 +0200

    autoswitch errxecx

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
 import Data.Roman
+import Text.Read ( readMaybe ) 
 
 main = do  
     putStrLn "whats ur numbaa"  
     x <- getLine  
-    putStrLn (show (fromInteger (read x ::Integer)::RomanNumeral)  )
-
+    let test = readMaybe x ::Maybe Integer
+    if test == Nothing
+    then putStrLn (show (fromRoman (read x ::RomanNumeral) ::Integer)  )
+    else putStrLn (show (fromInteger (read x ::Integer) ::RomanNumeral))
 
 
